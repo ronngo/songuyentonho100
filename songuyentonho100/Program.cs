d550854168cd884337ad4739b2b6a051c2042fef
ronngo <70925598+ronngo@users.noreply.github.com>
2020-09-10 22:08:38 +0700

    Add files via upload

---
+++ songuyentonho100/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace songuyentonhohon100
+{
+    class Program
+    {
+        public static void sntnhohon100()
+        {
+            int a = 2;
+            while (a < 100)
+            {
+                bool n = true;
+
+                if (a < 2)
+                {
+                    n = false;
+                }
+                else
+                {
+                    int i = 2;
+
+                    while (i <= Math.Sqrt(a))
+                    {
+                        if (a % i == 0)
+                        {
+                            n = false;
+                            break;
+                        }
+                        i = i + 1;
+                    }
+                }
+                if (n)
+                    Console.Write("{0} ", a);
+                a++;
+
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("cac so nguyen to nho hon 100 la: ");
+            sntnhohon100();
+        }
+    }
+}
